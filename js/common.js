66c18da287273599e06da2b5efca4698d62673f2
김성은 <kse012050@naver.com>
2023-10-16 22:16:00 +0900

    애니메이션 진행 중

--- js/common.js
+++ js/common.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
     // css index
     $('[data-styleIdx]').length && styleIdx();
+
+    $(window).scroll(function(){
+        const scrollTop = $(this).scrollTop();
+        $('[data-animate]').each(function(){
+            if($(this).offset().top - $(window).height() / 1.3 < scrollTop){
+                $(this).addClass('ani');
+            }
+        })
+    })
+
+    $('[data-tab="click"] li button').click(function(){
+        $('[data-tab="click"] li').removeClass('active');
+        $('[data-tab="click"] li div').stop().slideUp();
+        $(this).parent().addClass('active');
+        $(this).siblings().stop().slideDown();
+        $('[data-tab="contents"]').children().removeClass('active');
+        $('[data-tab="contents"]').children().eq($(this).parent().index()).addClass('active');
+    })
 })
 
 // css index
